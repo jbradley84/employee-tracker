1ce76f92d27c24787679b792c2c56f12408592ae
jbradley84 <utbradley84@yahoo.com>
2022-07-23 09:43:30 -0500

    added switch statement promise to inquirer menu prompt

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
+const { allowedNodeEnvironmentFlags } = require('process');
 
 // connect to database
 const db = mysql.createConnection({
    host: "localhost",
-   port: 3001,
+   //port: 3001,
    user: "root",
    password: "9)goR9K%))*B9$6PVqDz",
    database: "human_resources",
@@ -32,4 +33,34 @@ function hr_menu() {
          "QUIT"
       ],
    })
+   .then(function (choices) {
+      switch (choices.action) {
+         case "View All Employees":
+            allEmployees();
+            break;
+         case "Add Employee":
+            addEmployee();
+            break;
+         case "Update Employee Role":
+            updateRole();
+            break;
+         case "View All Roles":
+            allRoles();
+            break;
+         case "Add Role":
+            addRole();
+            break;
+         case "View All Departments":
+            allDepartments();
+            break;
+         case "Add Department":
+            addDepartment();
+            break;
+         case "QUIT":
+            hrQuit();
+            break;
+         default:
+            break;
+      }
+   });
 }
